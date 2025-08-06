4492e1aab6fff9a7aa2317d2fae77f151617da42
Heigen007 <67007974+Heigen007@users.noreply.github.com>
2025-08-06 21:47:44 +0500

    Обновление

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,7 +2,10 @@ const config = {
     appId: 'org.repliky.quasar.app',
     appName: 'Repliky',
     webDir: 'dist/capacitor',
-    bundledWebRuntime: false
+    bundledWebRuntime: false,
+
+    // 🔹 Цвет фона WebView (safe area полосы)
+    backgroundColor: '#091a2c',
 }
 
 export default config
