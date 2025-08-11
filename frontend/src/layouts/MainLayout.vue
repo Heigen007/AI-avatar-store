93bb0e6cf6c6a41cf3765275bc6f762f57085d57
Heigen007 <67007974+Heigen007@users.noreply.github.com>
2025-08-11 16:48:02 +0500

    updates

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,9 +1,6 @@
 <template>
-  <q-layout view="lHh Lpr lFf" class="safe-area-top safe-area-bottom">
-    <q-page-container
-      class="relative myLayout overflow-hidden bg-[#0a1c2f]"
-      style="padding-bottom: calc(env(safe-area-inset-bottom) + 56px)"
-    >
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container class="safe-area-top safe-area-bottom relative myLayout overflow-hidden bg-[#0a1c2f]" style="padding-bottom: 0; height: 100vh">
       <router-view v-slot="{ Component, route }">
         <transition :name="transitionName">
           <div v-if="isUnderMaintenance" class="absolute inset-0 w-full h-full flex flex-col items-center justify-center text-center text-white px-6">
@@ -66,12 +63,6 @@ watch(() => route.path, (toPath) => {
 const isUnderMaintenance = computed(() => UserProfile.currentUser === null)
 </script>
 
-<style>
-.myLayout {
-  padding-top: env(safe-area-inset-top);
-  padding-bottom: env(safe-area-inset-bottom);
-}
-</style>
 
 <style scoped>
 /* Slide Left */
